0e60ac79300813782d95e305e48aada6470e189a
Enes Furkan Olcay <fopeak@gmail.com>
2019-01-06 22:20:54 +0300

    udp sockets

--- recorder.py
+++ recorder.py
@@ -1,23 +1,27 @@
 import pyaudio
 import wave
+import socket
+import sys
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 
+UDP_IP = "127.0.0.1"
+UDP_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True,frames_per_buffer=CHUNK)
+stream = p.open(format=FORMAT,channels=CHANNELS,rate=RATE,input=True, output=True, frames_per_buffer=CHUNK)
 print("recording.")
 
 frames = []
 
-for i in range(0, int(RATE / CHUNK * 15)):
+for i in range(0, int(RATE / CHUNK * 10)):
     data = stream.read(CHUNK)
-    frames.append(data)
-
-print(frames) # i know.
+    sock.sendto(data, (UDP_IP, UDP_PORT))
 
 stream.stop_stream()
 stream.close()
